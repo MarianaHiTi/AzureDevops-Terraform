d3e2de16de8dbb6e47f09d0eefd4eeb1e332ea4f
Mariana Magdalena Hinojosa Tijerina <hino.tije@gmail.com>
2022-04-01 10:57:00 -0600

    Change values forecast just to valid the test

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace weatherapi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "xxx", "Bracing", "xxx", "yyy", "Mild", "zzz", "Balmy", "yyy", "Sweltering", "zzz"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
